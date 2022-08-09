e6d8d318f8a22f3110bbf402a14a51a8add1da0b
maybe <iammaybe7@gmail.com>
2022-08-09 16:57:19 +0200

    Add 'setTime' function

--- Course 4/Section 10/Project 9 - Counter App/js/script.js
+++ Course 4/Section 10/Project 9 - Counter App/js/script.js
@@ -1,5 +1,5 @@
 const settings = document.querySelector('.settings');
-const settingsBtn = document.querySelector('.settings');
+const settingsBtn = document.querySelector('.settings-btn');
 const imageSection = document.querySelector('.image-section');
 
 const eventName = document.querySelector('#event-name');
@@ -17,15 +17,33 @@ const saveBtn = document.querySelector('.save');
 const eventSpan = document.querySelector('.event');
 let usersTime;
 
+const setTime = () => {
+  const currentTime = new Date();
+  const result = usersTime - currentTime;
+
+  const days = Math.floor(result / 1000 / 60 / 60 / 24);
+  const hours = Math.floor(result / 1000 / 60 / 60) % 24;
+  const minutes = Math.floor(result / 1000 / 60) % 60;
+  const seconds = Math.floor(result / 1000) % 60;
+
+  daysCount.textContent = days;
+  hoursCount.textContent = hours;
+  minutesCount.textContent = minutes;
+  secondsCount.textContent = seconds;
+};
+
 const appUpdate = () => {
   eventSpan.textContent = eventName.value;
   usersTime = new Date(
     `${eventDay.value} ${eventMonth.value} ${eventYear.value}`
   );
   imageSection.style.backgroundImage = `url('${eventImg.value}')`;
+  setTime();
 };
 
 settingsBtn.addEventListener('click', () => {
   settings.classList.toggle('active');
 });
 saveBtn.addEventListener('click', appUpdate);
+appUpdate();
+setInterval(setTime, 1000);
