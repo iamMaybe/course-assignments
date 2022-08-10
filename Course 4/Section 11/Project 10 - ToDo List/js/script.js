3ad18242af2f36cd8c1d7b78cec3c333378ce936
maybe <iammaybe7@gmail.com>
2022-08-10 14:42:08 +0200

    Get elements and add 'main' function

---
+++ Course 4/Section 11/Project 10 - ToDo List/js/script.js
@@ -0,0 +1,35 @@
+let todoInput;
+let alertInfo;
+let addBtn;
+let ulList;
+let newTask;
+let allTasks;
+let idNumber = 0;
+let popup;
+let popupInfo;
+let editedTodo;
+let popupInput;
+let addPopupBtn;
+let closeTodoBtn;
+
+const main = () => {
+  prepareDOMElements();
+  prepareDOMEvents();
+};
+
+const prepareDOMElements = () => {
+  todoInput = document.querySelector('.todo-input');
+  alertInfo = document.querySelector('.alert-info');
+  addBtn = document.querySelector('.add-btn');
+  ulList = document.querySelector('.todo-list ul');
+  allTasks = document.querySelector('li');
+  popup = document.querySelector('popup');
+  popupInfo = document.querySelector('.popup-info');
+  popupInput = document.querySelector('.popup-input');
+  addPopupBtn = document.querySelector('.accept');
+  closeTodoBtn = document.querySelector('.cancel');
+};
+
+const prepareDOMEvents = () => {};
+
+document.addEventListener('DOMContentLoaded', main);
