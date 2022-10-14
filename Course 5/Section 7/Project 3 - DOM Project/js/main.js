55bb0d030efb712b18a3563d13e03c67e996f906
maybe <iammaybe7@gmail.com>
2022-10-14 13:49:51 +0200

    Add 'searchElements' function

--- Course 5/Section 7/Project 3 - DOM Project/js/main.js
+++ Course 5/Section 7/Project 3 - DOM Project/js/main.js
@@ -1,18 +1,30 @@
 const addForm = document.querySelector('.form--add');
 const searchForm = document.querySelector('.form--search');
+const result = document.querySelector('.result');
 const content = document.querySelector('.content');
 const elInput = document.querySelector('.el');
 const txtInput = document.querySelector('.txt');
 const attrInput = document.querySelector('.attr');
 const valueInput = document.querySelector('.value');
+const searchInput = document.querySelector('.search');
 
 const addElement = (e, el, txt, attr, value) => {
   e.preventDefault();
   const element = document.createElement(el);
-
   if (txt) element.textContent = txt;
   if (attr) element.setAttribute(attr, value);
   content.append(element);
+};
+
+const searchElements = (e, searchedElement) => {
+  e.preventDefault();
+  const elements = document.querySelectorAll(searchedElement);
+
+  if (elements.length) {
+    result.innerHTML = `<p class="result__info">W tym dokumencie znalazłem <strong>${elements.length}</strong> elementów <strong>${searchedElement}</strong>.</p>`;
+  } else {
+    result.innerHTML = `<p class="result__info">W tym dokumencie nie znalazłem elementów <strong>${searchedElement}</strong>.</p>`;
+  }
 };
 
 addForm.addEventListener('submit', e =>
@@ -23,4 +35,8 @@ addForm.addEventListener('submit', e =>
     attrInput.value,
     valueInput.value
   )
+);
+
+searchForm.addEventListener('submit', e =>
+  searchElements(e, searchInput.value)
 );
