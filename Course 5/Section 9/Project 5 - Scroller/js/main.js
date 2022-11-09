31f23b9a5bf77a521052cb0d713bdab11154f8e0
maybe <iammaybe7@gmail.com>
2022-11-09 21:38:10 +0100

    feat: add listeners for swipeUp and swipeDown

--- Course 5/Section 9/Project 5 - Scroller/js/main.js
+++ Course 5/Section 9/Project 5 - Scroller/js/main.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const scroller = new Scroller('.main');
-  document.addEventListener('wheel', scroller.listenScroll);
+document.addEventListener('DOMContentLoaded', function () {
+  const scroller = new Scroller('.root');
+
+  document.addEventListener('wheel', e => scroller.listenScroll(e));
+  document.addEventListener('swipeUp', () => scroller.scroll(1));
+  document.addEventListener('swipeDown', () => scroller.scroll(-1));
 });
