ee336849ecdf344c3fa79ac65d50acbed097324d
maybe <iammaybe7@gmail.com>
2022-11-09 22:11:48 +0100

    feat: add support for keyboard arrow keys

--- Course 5/Section 9/Project 5 - Scroller/js/main.js
+++ Course 5/Section 9/Project 5 - Scroller/js/main.js
@@ -4,4 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('wheel', e => scroller.listenScroll(e));
   document.addEventListener('swipeUp', () => scroller.scroll(1));
   document.addEventListener('swipeDown', () => scroller.scroll(-1));
+  document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowDown') scroller.scroll(1);
+    if (e.key === 'ArrowUp') scroller.scroll(-1);
+  });
 });
