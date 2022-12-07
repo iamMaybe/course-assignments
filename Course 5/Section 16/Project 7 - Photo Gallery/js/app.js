79b5d84fb2cca571781c800dfdb2a613cf46492c
maybe <iammaybe7@gmail.com>
2022-12-07 23:05:52 +0100

    refactor: rename imgEl element selector

--- Course 5/Section 16/Project 7 - Photo Gallery/js/app.js
+++ Course 5/Section 16/Project 7 - Photo Gallery/js/app.js
@@ -3,7 +3,7 @@ import '../sass/style.scss';
 class Dog {
   constructor() {
     this.apiUrl = 'https://dog.ceo/api';
-    this.imgEl = document.querySelector('.featured-dog-img');
+    this.imgEl = document.querySelector('.featured-dog__image img');
     this.init();
   }
 
