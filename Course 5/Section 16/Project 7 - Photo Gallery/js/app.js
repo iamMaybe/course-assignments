42bcb820c5f804d53986063cfde582a715f22b0f
maybe <iammaybe7@gmail.com>
2022-12-07 20:17:02 +0100

    feat: add listBreeds method

--- Course 5/Section 16/Project 7 - Photo Gallery/js/app.js
+++ Course 5/Section 16/Project 7 - Photo Gallery/js/app.js
@@ -5,4 +5,10 @@ class Dog {
     this.apiUrl = 'https://dog.ceo/api';
     this.imgEl = document.querySelector('.featured-dog-img');
   }
+
+  listBreeds() {
+    return fetch(`${this.apiUrl}/breeds/list/all`)
+      .then(res => res.json())
+      .then(data => data.message);
+  }
 }
