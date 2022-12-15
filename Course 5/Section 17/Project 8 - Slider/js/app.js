1580205ffb91899afdf88794cea8c032eca561f3
maybe <iammaybe7@gmail.com>
2022-12-15 12:02:25 +0100

    feat: add adjustImagesSize function

--- Course 5/Section 17/Project 8 - Slider/js/app.js
+++ Course 5/Section 17/Project 8 - Slider/js/app.js
@@ -1,1 +1,16 @@
 import '../sass/style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const imagesContainer = document.querySelector('.slider__images-container');
+  imgFirst = document.querySelector('.slider__image-img--first');
+  imgSecond = document.querySelector('.slider__image-img--second');
+
+  const adjustImagesSize = () => {
+    const imagesContainerWidth = imagesContainer.offsetWidth;
+    imgFirst.style.width = imagesContainerWidth + 'px';
+    imgSecond.style.width = imagesContainerWidth + 'px';
+  };
+
+  addEventListener('resize', adjustImagesSize);
+  adjustImagesSize();
+});
