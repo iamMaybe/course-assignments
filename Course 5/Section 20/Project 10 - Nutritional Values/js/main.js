9b50989789c890b4715b6f5cb35e0dd79fba5ece
maybe <iammaybe7@gmail.com>
2022-12-28 15:08:56 +0100

    feat: add values array

--- Course 5/Section 20/Project 10 - Nutritional Values/js/main.js
+++ Course 5/Section 20/Project 10 - Nutritional Values/js/main.js
@@ -1,1 +1,8 @@
 import '../sass/style.scss';
+
+const values = [
+  { name: 'Pineapple', calories: 41, fat: 0.1, carbs: 11, protein: 0.5 },
+  { name: 'Apple', calories: 95, fat: 0.3, carbs: 25, protein: 0.5 },
+  { name: 'Orange', calories: 69, fat: 0.2, carbs: 18, protein: 1.3 },
+  { name: 'Cherry', calories: 5.2, fat: 0, carbs: 1.3, protein: 0.1 },
+];
