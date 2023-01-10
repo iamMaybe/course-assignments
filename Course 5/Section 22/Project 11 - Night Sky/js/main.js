870024bf53686260a17de03cb98428274bd55d1b
maybe <iammaybe7@gmail.com>
2023-01-10 22:36:11 +0100

    feat: add initCanvas method

--- Course 5/Section 22/Project 11 - Night Sky/js/main.js
+++ Course 5/Section 22/Project 11 - Night Sky/js/main.js
@@ -5,6 +5,13 @@ class Sky {
     this.width = innerWidth;
     this.height = innerHeight;
   }
+
+  initCanvas() {
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.ctx.fillStyle = '#000';
+    this.ctx.fillRect(0, 0, this.width, this.height);
+  }
 }
 
 const sky = new Sky(document.querySelector('#canvas'));
