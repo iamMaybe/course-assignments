dddfda38c35faa4f6af0c3b1548ac45793537a9b
maybe <iammaybe7@gmail.com>
2023-01-12 19:09:02 +0100

    feat: add drawStar method

--- Course 5/Section 22/Project 11 - Night Sky/js/main.js
+++ Course 5/Section 22/Project 11 - Night Sky/js/main.js
@@ -13,6 +13,24 @@ class Sky {
     this.ctx.fillRect(0, 0, this.width, this.height);
   }
 
+  drawStar(star) {
+    this.ctx.save();
+    this.ctx.fillStyle = star.color;
+    this.ctx.beginPath();
+    this.ctx.translate(star.x, star.y);
+    this.ctx.moveTo(0, 0 - star.radius);
+
+    for (let i = 0; i < 5; i++) {
+      this.ctx.rotate((Math.PI / 180) * 36);
+      this.ctx.lineTo(0, 0 - star.radius * 0.65);
+      this.ctx.rotate((Math.PI / 180) * 36);
+      this.ctx.lineTo(0, 0 - star.radius);
+    }
+
+    this.ctx.fill();
+    this.ctx.restore();
+  }
+
   draw() {
     requestAnimationFrame(() => this.draw());
   }
