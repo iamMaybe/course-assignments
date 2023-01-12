a34483f3f58fbcd45507ecd92c7a32b6b03ab5b1
maybe <iammaybe7@gmail.com>
2023-01-12 23:39:17 +0100

    feat: add generateStars method

--- Course 5/Section 22/Project 11 - Night Sky/js/main.js
+++ Course 5/Section 22/Project 11 - Night Sky/js/main.js
@@ -13,6 +13,22 @@ class Sky {
     this.ctx.fillRect(0, 0, this.width, this.height);
   }
 
+  generateStars(count) {
+    let stars = [];
+
+    for (let i = 0; i < count; i++) {
+      const radius = Math.random() * 3 + 2;
+      stars.push({
+        x: Math.random() * this.width,
+        y: Math.random() * this.height,
+        radius: radius,
+        color: '#fff',
+      });
+    }
+
+    this.stars = [];
+  }
+
   drawStar(star) {
     this.ctx.save();
     this.ctx.fillStyle = star.color;
