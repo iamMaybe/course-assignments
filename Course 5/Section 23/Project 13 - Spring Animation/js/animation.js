8a38783a435d0c334b780bf065f40d13504baf9a
maybe <iammaybe7@gmail.com>
2023-02-06 14:55:31 +0100

    feat: add animation support for app elements

--- Course 5/Section 23/Project 13 - Spring Animation/js/animation.js
+++ Course 5/Section 23/Project 13 - Spring Animation/js/animation.js
@@ -1,16 +1,38 @@
 const ball = document.querySelector('.ball');
 const btn = document.querySelector('.btn-action');
 const spring = document.querySelector('.spring');
+const bar = document.querySelector('.bar');
 const fill = document.querySelector('.fill');
 
 const stretchSpring = () => {
   fill.style.animationPlayState = 'running';
   spring.style.animationPlayState = 'running';
   btn.textContent = 'Release the spring!';
+  btn.removeEventListener('mousedown', stretchSpring);
+  btn.removeEventListener('touchstart', stretchSpring);
 };
 
 const releaseSpring = () => {
-  console.log('release');
+  const fillStyles = getComputedStyle(fill);
+  const barStyles = getComputedStyle(bar);
+  const springStyles = getComputedStyle(spring);
+  const fillWidth = parseInt(fillStyles.width, 10);
+  const barWidth = parseInt(barStyles.width, 10);
+
+  const progressPercent = (fillWidth / barWidth).toFixed(2);
+  btn.style.color = '#000';
+  btn.textContent = `Power: ${(progressPercent * 100).toFixed()}%`;
+  fill.style.animationPlayState = 'paused';
+
+  document.documentElement.style.setProperty('--power-x', `${30 + progressPercent * 50}%`);
+  ball.style.animation =
+    'fly-ball-x 1s .15s 1 forwards cubic-bezier(0.17, 0.67, 0.6, 1), fly-ball-y 1s .15s 1 forwards linear';
+
+  document.documentElement.style.setProperty('--spring-left', `${springStyles.left}`);
+  spring.style.animation = 'release-spring .2s 1 forwards linear';
+
+  btn.removeEventListener('mouseup', stretchSpring);
+  btn.removeEventListener('touchend', stretchSpring);
 };
 
 const resetAnimation = () => {
