608f12f68c87a729eb3d86bebee9550b44d600eb
maybe <iammaybe7@gmail.com>
2023-02-11 00:42:27 +0100

    feat: add multiple text display

--- Course 5/Section 24/Project 14 - Typing Animation/js/typing.js
+++ Course 5/Section 24/Project 14 - Typing Animation/js/typing.js
@@ -1,13 +1,22 @@
 const box = document.querySelector('.typing');
-const text = 'Hello! How are you?';
+const text = ['Hello! How was your day?', 'My day was great!', 'But tell me how you are doing!'];
 let letterIndex = 0;
+let textIndex = 0;
 let oldTime = 0;
 let speed = 100;
 
 const typing = newTime => {
+  if (letterIndex === text[textIndex].length) {
+    return setTimeout(() => {
+      box.textContent = '';
+      textIndex++;
+      letterIndex = 0;
+      requestAnimationFrame(typing);
+    }, 2000);
+  }
   if (newTime - oldTime > speed) {
     oldTime = newTime;
-    const letter = text.charAt(letterIndex++);
+    const letter = text[textIndex].charAt(letterIndex++);
     box.textContent += letter;
   }
   requestAnimationFrame(typing);
