9983fde9276c973f6164843be1e0652280c37621
maybe <iammaybe7@gmail.com>
2023-02-10 23:54:40 +0100

    feat: add animation speed feature

--- Course 5/Section 24/Project 14 - Typing Animation/js/typing.js
+++ Course 5/Section 24/Project 14 - Typing Animation/js/typing.js
@@ -1,10 +1,16 @@
 const box = document.querySelector('.typing');
 const text = 'Hello! How are you?';
 let letterIndex = 0;
+let oldTime = 0;
+let speed = 100;
 
-const typing = () => {
-  const letter = text.charAt(letterIndex++);
-  box.textContent += letter;
+const typing = newTime => {
+  if (newTime - oldTime > speed) {
+    oldTime = newTime;
+    const letter = text.charAt(letterIndex++);
+    box.textContent += letter;
+  }
+  requestAnimationFrame(typing);
 };
 
 typing();
