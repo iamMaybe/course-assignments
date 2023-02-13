37690ee5a9ad9a8629f2b8ff8adc6ad27b839439
maybe <iammaybe7@gmail.com>
2023-02-13 21:39:43 +0100

    feat: add paragraph break using special character

--- Course 5/Section 24/Project 14 - Typing Animation/js/typing.js
+++ Course 5/Section 24/Project 14 - Typing Animation/js/typing.js
@@ -1,5 +1,9 @@
 const box = document.querySelector('.typing');
-const text = ['Hello! How was your day?', 'My day was great!', 'But tell me how you are doing!'];
+const text = [
+  'Hello! How was your day?^My day was great!',
+  'I had a lot of work!^I used all my algorithms!',
+  "I hope you're okay!^Beeeeep!",
+];
 let letterIndex = 0;
 let textIndex = 0;
 let oldTime = 0;
@@ -8,6 +12,8 @@ let activeDOMElement = box;
 
 const typing = newTime => {
   if (newTime - oldTime > speed) {
+    const letter = text[textIndex].charAt(letterIndex);
+
     if (letterIndex === text[textIndex].length) {
       if (textIndex === text.length - 1) return;
       return setTimeout(() => {
@@ -16,16 +22,19 @@ const typing = newTime => {
         letterIndex = 0;
         requestAnimationFrame(typing);
       }, 1000);
-    } else if (letterIndex === 0) {
+    } else if (letterIndex === 0 || letter === '^') {
       const p = document.createElement('p');
       p.classList.add('typing__text');
       box.append(p);
       activeDOMElement = p;
     }
 
+    if (letter !== '^') {
+      activeDOMElement.textContent += letter;
+    }
+
     oldTime = newTime;
-    const letter = text[textIndex].charAt(letterIndex++);
-    activeDOMElement.textContent += letter;
+    letterIndex++;
   }
   requestAnimationFrame(typing);
 };
