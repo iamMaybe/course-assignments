1b00a20c773cd49c3888c8611bfba7e0caa3ed4c
maybe <iammaybe7@gmail.com>
2023-03-01 23:59:37 +0100

    refactor: use newer notation for ease

--- Course 5/Section 25/Project 15 - Bouncing Ball Animation (GSAP)/js/animation.js
+++ Course 5/Section 25/Project 15 - Bouncing Ball Animation (GSAP)/js/animation.js
@@ -2,5 +2,5 @@ const ball = document.querySelector('.ball');
 const ballHeight = ball.offsetHeight;
 const height = window.innerHeight - ballHeight;
 
-gsap.to(ball, { duration: 5, y: height, ease: Bounce.easeOut });
+gsap.to(ball, { duration: 5, y: height, ease: 'bounce.out' });
 gsap.to(ball, { duration: 2, backgroundColor: 'green', scale: 0, delay: 5 });
