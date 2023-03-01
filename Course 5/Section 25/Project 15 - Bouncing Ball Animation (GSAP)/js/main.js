8ea45ab7628fe04274c4395973ba4af3fcfc7b0e
maybe <iammaybe7@gmail.com>
2023-03-01 13:04:51 +0100

    refactor: replace TweenMax.to with newer gsap.to

--- Course 5/Section 25/Project 15 - Bouncing Ball Animation (GSAP)/js/main.js
+++ Course 5/Section 25/Project 15 - Bouncing Ball Animation (GSAP)/js/main.js
@@ -2,5 +2,5 @@ const ball = document.querySelector('.ball');
 const ballHeight = ball.offsetHeight;
 const height = window.innerHeight - ballHeight;
 
-TweenMax.to(ball, 5, { y: height, ease: Bounce.easeOut });
-TweenMax.to(ball, 2, { backgroundColor: 'green', scale: 0, delay: 5 });
+gsap.to(ball, { duration: 5, y: height, ease: Bounce.easeOut });
+gsap.to(ball, { duration: 2, backgroundColor: 'green', scale: 0, delay: 5 });
