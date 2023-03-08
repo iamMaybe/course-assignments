a6a4d15cd0eab8eeb1759e18e1138d36e59b7202
maybe <iammaybe7@gmail.com>
2023-03-08 23:55:02 +0100

    feat: add properties applied before animation

--- Course 5/Section 25/Project 16 - Squares Animation (GSAP)/js/animation.js
+++ Course 5/Section 25/Project 16 - Squares Animation (GSAP)/js/animation.js
@@ -1,2 +1,3 @@
+gsap.set('.square', { visibility: 'visible', width: 300, border: '2px solid red' });
 gsap.from('.square__first', { duration: 2, x: 0, repeat: -1, yoyo: true, ease: 'back.out(3)' });
 gsap.fromTo('.square__second', { y: 100 }, { y: 300, duration: 4 });
