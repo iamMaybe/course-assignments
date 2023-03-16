81086209ae999fa502332280b72bea10e1cfe66f
maybe <iammaybe7@gmail.com>
2023-03-16 15:52:00 +0100

    feat: add animation of sunrays changing size

--- Course 5/Section 25/Project 21 - Sun (GSAP & SVG)/js/animation.js
+++ Course 5/Section 25/Project 21 - Sun (GSAP & SVG)/js/animation.js
@@ -1,4 +1,8 @@
 const streaks = document.querySelector('#streaks');
+const polygons = document.querySelectorAll('#streaks polygon');
 
 gsap.set(streaks, { transformOrigin: '50%', scale: 0.7 });
 gsap.to(streaks, { duration: 2, rotation: 360, repeat: -1, ease: 'none' });
+
+gsap.set(polygons, { transformOrigin: '50%', scale: 0.6 });
+gsap.to(polygons, { duration: 1, scale: 1.2, repeat: -1, yoyo: true, stagger: 0.3 });
