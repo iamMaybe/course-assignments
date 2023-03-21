1ab1335e9ead78724c935308b2514694f81d838b
maybe <iammaybe7@gmail.com>
2023-03-21 23:57:02 +0100

    refactor: use newer stagger property notation

--- Course 5/Section 25/Project 21 - Sun (GSAP & SVG)/js/animation.js
+++ Course 5/Section 25/Project 21 - Sun (GSAP & SVG)/js/animation.js
@@ -5,4 +5,4 @@ gsap.set(streaks, { transformOrigin: '50%', scale: 0.7 });
 gsap.to(streaks, { duration: 2, rotation: 360, repeat: -1, ease: 'none' });
 
 gsap.set(polygons, { transformOrigin: '50%', scale: 0.6 });
-gsap.to(polygons, { duration: 1, scale: 1.2, repeat: -1, yoyo: true, stagger: 0.3 });
+gsap.to(polygons, { duration: 1, scale: 1.2, stagger: { each: 0.3, repeat: -1, yoyo: true } });
