592d11fe66a928a070830ccf7cf7c73a8464c61b
maybe <iammaybe7@gmail.com>
2023-03-23 01:16:15 +0100

    feat: add solution using promises

--- Course 6/Section 2/Assignment 1/js/index.js
+++ Course 6/Section 2/Assignment 1/js/index.js
@@ -1,27 +1,61 @@
-// 1. CALLBACK
+// // 1. CALLBACK
+// const boilWater = callback => {
+//   console.log('Boiling water...');
+//   setTimeout(callback, 1000);
+// };
 
-const boilWater = callback => {
+// const makeATea = callback => {
+//   console.log('Tea brewing...');
+//   setTimeout(callback, 2000);
+// };
+
+// const waitForRightTemperature = callback => {
+//   console.log('Waiting for the right temperature...');
+//   setTimeout(callback, 750);
+// };
+
+// boilWater(() => {
+//   console.log('Water boiled!');
+//   makeATea(() => {
+//     console.log('Tea brewed!');
+
+//     waitForRightTemperature(() => {
+//       console.log('The temperature is right, time to drink!');
+//     });
+//   });
+// });
+
+// 2. PROMISE
+const boilWater = () => {
   console.log('Boiling water...');
-  setTimeout(callback, 1000);
-};
-
-const makeATea = callback => {
-  console.log('Tea brewing...');
-  setTimeout(callback, 2000);
-};
-
-const waitForRightTemperature = callback => {
-  console.log('Waiting for the right temperature...');
-  setTimeout(callback, 750);
-};
-
-boilWater(() => {
-  console.log('Water boiled!');
-  makeATea(() => {
-    console.log('Tea brewed!');
-
-    waitForRightTemperature(() => {
-      console.log('The temperature is right, time to drink!');
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000);
   });
-});
+};
+
+const makeATea = () => {
+  console.log('Tea brewing...');
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000);
+  });
+};
+
+const waitForRightTemperature = () => {
+  console.log('Waiting for the right temperature...');
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 750);
+  });
+};
+
+boilWater()
+  .then(() => {
+    console.log('Water boiled!');
+    return makeATea();
+  })
+  .then(() => {
+    console.log('Tea brewed!');
+    return waitForRightTemperature();
+  })
+  .then(() => {
+    console.log('The temperature is right, time to drink!');
+  });
