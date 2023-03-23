1d04b36c144a0ae1455ce909f91d68ac527a998d
maybe <iammaybe7@gmail.com>
2023-03-23 12:03:53 +0100

    feat: add solution using async/await

--- Course 6/Section 2/Assignment 1/js/index.js
+++ Course 6/Section 2/Assignment 1/js/index.js
@@ -25,7 +25,42 @@
 //   });
 // });
 
-// 2. PROMISE
+// // 2. PROMISE
+// const boilWater = () => {
+//   console.log('Boiling water...');
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, 1000);
+//   });
+// };
+
+// const makeATea = () => {
+//   console.log('Tea brewing...');
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000);
+//   });
+// };
+
+// const waitForRightTemperature = () => {
+//   console.log('Waiting for the right temperature...');
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, 750);
+//   });
+// };
+
+// boilWater()
+//   .then(() => {
+//     console.log('Water boiled!');
+//     return makeATea();
+//   })
+//   .then(() => {
+//     console.log('Tea brewed!');
+//     return waitForRightTemperature();
+//   })
+//   .then(() => {
+//     console.log('The temperature is right, time to drink!');
+//   });
+
+// 3. ASYNC/AWAIT
 const boilWater = () => {
   console.log('Boiling water...');
   return new Promise((resolve, reject) => {
@@ -47,15 +82,11 @@ const waitForRightTemperature = () => {
   });
 };
 
-boilWater()
-  .then(() => {
-    console.log('Water boiled!');
-    return makeATea();
-  })
-  .then(() => {
-    console.log('Tea brewed!');
-    return waitForRightTemperature();
-  })
-  .then(() => {
-    console.log('The temperature is right, time to drink!');
-  });
+(async () => {
+  await boilWater();
+  console.log('Water boiled!');
+  await makeATea();
+  console.log('Tea brewed!');
+  await waitForRightTemperature();
+  console.log('The temperature is right, time to drink!');
+})();
