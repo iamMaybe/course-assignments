4259a1daaa75390b2d4e87ac3b26bbaa22fa0e5b
maybe <iammaybe7@gmail.com>
2023-03-24 14:02:57 +0100

    feat: add solution using promises

--- Course 6/Section 3/Assignment 1/js/index.js
+++ Course 6/Section 3/Assignment 1/js/index.js
@@ -1,25 +1,52 @@
-// 1. CALLBACK
-const doMyJob = (hours, callback) => {
-  if (hours > 8) {
-    callback(new Error('Too many working hours!'));
-  } else {
-    setTimeout(() => {
-      callback(null);
-    }, hours * 1000);
-  }
+// // 1. CALLBACK
+// const doMyJob = (hours, callback) => {
+//   if (hours > 8) {
+//     callback(new Error('Too many working hours!'));
+//   } else {
+//     setTimeout(() => {
+//       callback(null);
+//     }, hours * 1000);
+//   }
+// };
+
+// const pay = callback => {
+//   callback(null);
+// };
+
+// doMyJob(5, err => {
+//   if (err === null) {
+//     console.log('Task completed!');
+//     pay(() => {
+//       console.log('Payment made!');
+//     });
+//   } else {
+//     console.log('Error!', err);
+//   }
+// });
+
+// 2. PROMISE
+const doMyJob = hours => {
+  return new Promise((resolve, reject) => {
+    if (hours > 8) {
+      reject(new Error('Too many working hours!'));
+    } else {
+      setTimeout(resolve, hours * 1000);
+    }
+  });
 };
 
-const pay = callback => {
-  callback(null);
+const pay = () => {
+  return new Promise(resolve => resolve());
 };
 
-doMyJob(5, err => {
-  if (err === null) {
+doMyJob(4)
+  .then(() => {
     console.log('Task completed!');
-    pay(() => {
-      console.log('Payment made!');
-    });
-  } else {
+    return pay();
+  })
+  .then(() => {
+    console.log('Payment made!');
+  })
+  .catch(err => {
     console.log('Error!', err);
-  }
-});
+  });
