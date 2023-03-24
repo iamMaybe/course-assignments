e379d15c45e6ce204943034bdb4223246f5fe521
maybe <iammaybe7@gmail.com>
2023-03-24 14:14:19 +0100

    feat: add solution using async/await

--- Course 6/Section 3/Assignment 1/js/index.js
+++ Course 6/Section 3/Assignment 1/js/index.js
@@ -24,7 +24,34 @@
 //   }
 // });
 
-// 2. PROMISE
+// // 2. PROMISE
+// const doMyJob = hours => {
+//   return new Promise((resolve, reject) => {
+//     if (hours > 8) {
+//       reject(new Error('Too many working hours!'));
+//     } else {
+//       setTimeout(resolve, hours * 1000);
+//     }
+//   });
+// };
+
+// const pay = () => {
+//   return new Promise(resolve => resolve());
+// };
+
+// doMyJob(4)
+//   .then(() => {
+//     console.log('Task completed!');
+//     return pay();
+//   })
+//   .then(() => {
+//     console.log('Payment made!');
+//   })
+//   .catch(err => {
+//     console.log('Error!', err);
+//   });
+
+// 3. ASYNC/AWAIT
 const doMyJob = hours => {
   return new Promise((resolve, reject) => {
     if (hours > 8) {
@@ -39,14 +66,13 @@ const pay = () => {
   return new Promise(resolve => resolve());
 };
 
-doMyJob(4)
-  .then(() => {
+(async () => {
+  try {
+    await doMyJob(3);
     console.log('Task completed!');
-    return pay();
-  })
-  .then(() => {
+    await pay();
     console.log('Payment made!');
-  })
-  .catch(err => {
+  } catch (err) {
     console.log('Error!', err);
-  });
+  }
+})();
