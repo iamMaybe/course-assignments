d8fb75bb761e345a011d619dccb0ea1ff6af4285
maybe <iammaybe7@gmail.com>
2023-03-29 14:44:38 +0200

    chore: install gulp-autoprefixer and gulp-cssnano

--- Course 7/Section 4/Assignment 1/gulpfile.js
+++ Course 7/Section 4/Assignment 1/gulpfile.js
@@ -1,8 +1,14 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
 
 function sassCompiler(done) {
-  src('./src/sass/**/*.scss').pipe(sass().on('error', sass.logError)).pipe(dest('./dist/css'));
+  src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(dest('./dist/css'));
   done();
 }
 
