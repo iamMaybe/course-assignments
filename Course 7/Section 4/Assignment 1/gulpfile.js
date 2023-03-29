34f98b54317fd99323b8dd2421af02351f27729d
maybe <iammaybe7@gmail.com>
2023-03-29 14:59:21 +0200

    chore: install gulp-rename

--- Course 7/Section 4/Assignment 1/gulpfile.js
+++ Course 7/Section 4/Assignment 1/gulpfile.js
@@ -2,12 +2,14 @@ const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
 
 function sassCompiler(done) {
   src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
     .pipe(dest('./dist/css'));
   done();
 }
